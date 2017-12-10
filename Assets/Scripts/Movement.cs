e13267bce247d25bdcedd8245ea639954f8acd7c
StrattonZach <zach.stratton@outlook.com>
2017-12-10 16:04:14 +0100

    Initial
    Created the project.

---
+++ Assets/Scripts/Movement.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class Movement : MonoBehaviour {
+
+	public Transform target;
+	public NavMeshAgent agent;
+
+	public GameObject[] targets;
+
+	public float range = 3f;
+
+	// Use this for initialization
+	void Start () {
+		agent = GetComponent<NavMeshAgent>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        GetTarget();
+        MovetoTarget();
+	}
+
+	void GetTarget()
+	{
+		//Fancy logic to choose who to attack..
+		targets = GameObject.FindGameObjectsWithTag("Enemy");
+        target = targets[0].transform; 
+	}
+
+    private void MovetoTarget()
+    {
+        agent.stoppingDistance = range;
+        agent.destination = target.position;
+    }
+    
+}
